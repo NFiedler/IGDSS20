b312ba57175904ead5624cfe2fa336a4e12122b1
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2020-07-03 15:23:04 +0200

    new navigation

---
+++ Assets/Scripts/fieldcreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class fieldcreator
+{
+    Dictionary<Tile, int> tileweights;
+
+    public fieldcreator(Building b)
+    {
+        tileweights = new Dictionary<Tile, int>();
+        calcweight(b._tile, 0);
+    }
+
+    private int calcweight(Tile t, int prevWeight)
+    {
+        int weight = prevWeight
+        while (true)
+        {
+            foreach (Tile tile in t._neighborTiles)
+            {
+                
+                if (!tileweights.ContainsKey(tile))
+                {
+                    calcweight(tile, weight);
+                }
+            }
+
+
+        }
+    }
+}
